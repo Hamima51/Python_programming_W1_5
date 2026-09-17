8bfe4e43fcfac669b10d6b11c69d4eac57d3c9c2
Hamima Akter Lia <hamima010104@gmail.com>
2026-09-17 13:07:23 +0300

    w1_5 update

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ feed=input("Enter the width in meters: ")
 width=int(feed)
 feed=input("Enter the height in meters: ")
 height=int(feed)
-print(f"Width is {width} m and height is {height} m")
+print(f"Width is {width} m and height is {height} m.")
 area=(f"{width} * {height}")
 area=width*height
 print(f"The wall will be {area} square meters.")
